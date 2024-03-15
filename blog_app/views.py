26b39d417b2ef7581af2d9c1b9b17ac17e977403
Mohira12 <mohirainoyatova3@gmail.com>
2024-03-15 14:07:37 +0300

    15-mart 14:07

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,21 +1,43 @@
-import datetime
-import random
-
-from django.shortcuts import render, HttpResponse
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from blog_app.models import Blog  # new
+from .forms import CommentForm
+from .models import Comment
 
 
 # Create your views here.
 def index(request):
+    blogs = Blog.objects.all().order_by('-created')  # new
+
+    paginator = Paginator(blogs, 1)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        "name": "Mohira",
-        "age": random.randint(1, 25),
-        "time": datetime.datetime.now().strftime('%Y.%m.%d'),
-        "a": random.randint(1, 1000),
-        "b": random.randint(-100, 1000),
+        "blogs": page_obj
     }
-    c = context['a'] + context['b']
-    c1 = context['a'] * context['b']
-    return HttpResponse(f"""<h1 style: 'color: blue;'>{context["name"]} ning yoshi {context["age"]} da.</h1>"""
-                        f"""<p>Bugungi sana: <code>{context["time"]}</code></p>
-                        <p>{context['a']} + {context['b']} = {c}</p>
-                        <p>{context['a']} * {context['b']} = {c1}</p>""")
+    return render(request, 'blog/index.html', context=context)
+
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, id=pk)
+    next_article = Blog.objects.filter(pk__gt=pk).order_by('pk').first()
+    previous_article = Blog.objects.filter(pk__lt=pk).order_by('-pk').first()
+    # form connection
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.blog = blog
+            comment.save()
+            return redirect('/')
+    else:
+        form = CommentForm()
+
+    context = {
+        "blog": blog,
+        'next_article': next_article,
+        'previous_article': previous_article,
+        'form': form
+    }
+    return render(request, 'blog/detail.html', context=context)
